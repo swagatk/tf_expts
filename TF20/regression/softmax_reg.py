37f63de52b6231fd95447d8eb90c82d48b8bd1e4
Swagat Kumar <swagat.kumar@gmail.com>
2020-08-18 11:09:39 +0100

    update sofmax_reg.py file

--- TF20/regression/softmax_reg.py
+++ TF20/regression/softmax_reg.py
@@ -15,11 +15,10 @@ x2_label1 = np.random.normal(4, 1, (100, 1))
 x1_label2 = np.random.normal(8, 1, (100, 1))
 x2_label2 = np.random.normal(0, 1, (100, 1))
 
-# plt.scatter(x1_label0, x2_label0, c='r', marker='o', s=60)
-# plt.scatter(x1_label1, x2_label1, c='g', marker='x', s=60)
-# plt.scatter(x1_label2, x2_label2, c='b', marker='_', s=60)
-# plt.show()
-
+plt.scatter(x1_label0, x2_label0, c='r', marker='o', s=60)
+plt.scatter(x1_label1, x2_label1, c='g', marker='x', s=60)
+plt.scatter(x1_label2, x2_label2, c='b', marker='_', s=60)
+plt.show()
 
 # combines all data into a one big matrix
 xs_label0 = np.hstack((x1_label0, x2_label0))
@@ -44,10 +43,15 @@ test_x1_label1 = np.random.normal(5, 1, (10, 1))
 test_x2_label1 = np.random.normal(4, 1, (10, 1))
 test_x1_label2 = np.random.normal(8, 1, (10, 1))
 test_x2_label2 = np.random.normal(0, 1, (10, 1))
-test_xs_label0 = np.hstack((test_x1_label0, test_x2_label0))
-test_xs_label1 = np.hstack((test_x1_label1, test_x2_label1))
-test_xs_label2 = np.hstack((test_x1_label2, test_x2_label2))
-test_xs = np.vstack((test_xs_label0, test_xs_label1, test_xs_label2))
+test_xs_label0 = np.hstack((test_x1_label0,
+                            test_x2_label0))
+test_xs_label1 = np.hstack((test_x1_label1,
+                            test_x2_label1))
+test_xs_label2 = np.hstack((test_x1_label2,
+                            test_x2_label2))
+test_xs = np.vstack((test_xs_label0,
+                     test_xs_label1,
+                     test_xs_label2))
 test_labels = np.array([[1., 0., 0.]] * 10 +
                        [[0., 1., 0.]] * 10 +
                        [[0., 0., 1.]] * 10)
@@ -56,7 +60,7 @@ train_size, num_features = xs.shape
 
 ######################
 # define hyperparameters
-learning_rate = 0.01
+learning_rate = 0.001
 training_epochs = 1000
 num_labels = 3
 batch_size = 100
@@ -88,29 +92,64 @@ def accuracy(pred, y):
 # optimizer
 opt = tf.optimizers.SGD(learning_rate)
 
+# Create a Model and Train
 model = SoftmaxModel()
-# Train
 for step in range(training_epochs * train_size // batch_size):
     offset = (step * batch_size) % train_size
 
     batch_xs = xs[offset:(offset + batch_size), :]
-    # batch_xs = batch_xs.astype('float32')
     batch_labels = labels[offset:(offset + batch_size)]
-    # batch_labels = batch_labels.astype('float32')
 
     with tf.GradientTape() as tape:
         training_variables = [model.Weight, model.Bias]
         pred = model(batch_xs)
         current_loss = loss(pred, batch_labels)
-    gradients = tape.gradient(current_loss, training_variables)
-    opt.apply_gradients(zip(gradients, training_variables))
-
+        gradients = tape.gradient(current_loss, training_variables)
+        opt.apply_gradients(zip(gradients, training_variables))
     print(step, current_loss.numpy())
+
+# final values of weights and bias
+W = model.Weight.numpy()
+b = model.Bias.numpy()
+print('Weight: ', W)
+print('Bias: ', b)
 
 # Testing
 print('accuracy: ', accuracy(model(test_xs), test_labels).numpy())
 
-
+#
+# def sigmoid(x):
+#     return 1. / (1. + np.exp(-x))
+#
+#
+# # plotting
+# x1_boundary1, x2_boundary1 = [], []
+# x1_boundary2, x2_boundary2 = [], []
+# x1_boundary3, x2_boundary3 = [], []
+# for x1_test in np.linspace(-2, 10, 1000):
+#     for x2_test in np.linspace(-2, 10, 1000):
+#         z1 = sigmoid(-W[0][0] * x1_test - W[1][0] * x2_test - b[0])
+#         z2 = sigmoid(-W[0][1] * x1_test - W[1][1] * x2_test - b[1])
+#         z3 = sigmoid(-W[0][2] * x1_test - W[1][2] * x2_test - b[2])
+#
+#         if abs(z1 - 0.5) < 0.01:
+#             x1_boundary1.append(x1_test)
+#             x2_boundary1.append(x2_test)
+#         elif abs(z2 - 0.5) < 0.01:
+#             x1_boundary2.append(x1_test)
+#             x2_boundary2.append(x2_test)
+#         elif abs(z3 - 0.5) < 0.01:
+#             x1_boundary3.append(x1_test)
+#             x2_boundary3.append(x2_test)
+#
+#
+# plt.scatter(x1_boundary1, x2_boundary1, c='m', marker='o', s=20)
+# plt.scatter(x1_boundary2, x2_boundary2, c='k', marker='x', s=20)
+# plt.scatter(x1_boundary3, x2_boundary3, c='c', marker='+', s=20)
+# plt.xlabel('$x_1$')
+# plt.ylabel('$x_2$')
+# plt.show()
+#
 
 
 
